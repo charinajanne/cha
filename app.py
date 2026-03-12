073f20ca6d65ee8af02b0d8fee79ac73bae91543
charinajanne <charinajannebierneza@gmail.com>
2026-03-12 13:07:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,40 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# 1. Move the data into a dictionary variable so it can be modified
+student_database = {
+    "name": "Cha",
+    "grade": 10,
+    "section": "Arduino"
+}
+
 @app.route('/')
 def home():
     return "Welcome to my Flask API!"
 
-@app.route('/student')
+# 2. This route now returns the CURRENT state of the variable
+@app.route('/student', methods=['GET'])
 def get_student():
+    return jsonify(student_database)
+
+# 3. Add a route to UPDATE the data
+@app.route('/update-student', methods=['POST'])
+def update_student():
+    # Get the JSON data sent from the user/client
+    data = request.get_json()
+
+    # Update only the fields provided in the request
+    if 'name' in data:
+        student_database['name'] = data['name']
+    if 'grade' in data:
+        student_database['grade'] = data['grade']
+    if 'section' in data:
+        student_database['section'] = data['section']
+
     return jsonify({
-        "name": "Cha",
-        "grade": 10,
-        "section": "Arduino"
+        "message": "Student updated successfully!",
+        "new_data": student_database
     })
 
 if __name__ == '__main__':
-    # This block allows you to run the app directly using 'python app.py'
     app.run(debug=True)
-
